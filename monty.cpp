5e9d0d6da84afe3d59d69c524fd21e65b3d76823
Christian Catchpole <christian@catchpole.net>
2015-01-02 16:17:20 +1100

    initial commit

---
+++ monty.cpp
@@ -0,0 +1,440 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "monty.h"
+
+//
+// Monty SID synth for Raspberry Pi
+//
+// GPIO code is based on Jamie Nuttall's excelent SidPI project https://github.com/papawattu/SidPi
+//
+// todo: current implementation does not activate 1mhz clock. i'm still working on that.
+// todo: parametize midi device "/dev/snd/midiC1D0"
+//
+// gcc monty.cpp -o monty
+// sudo ./monty
+
+static unsigned char gpioToGPFSEL[] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5
+};
+
+// Define the shift up for the 3 bits per pin in each GPFSEL port
+static unsigned char gpioToShift[] = {
+    0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 0, 3, 6, 9, 12, 15, 18, 21, 24, 27,
+    0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 0, 3, 6, 9, 12, 15, 18, 21, 24, 27
+};
+
+static const int DATA[] = { 11, 9, 10, 22, 27, 17, 3, 2 };
+static const int ADDR[] = { 23, 24, 25, 8, 7 };
+
+static const unsigned short noteTable[] = {
+    // C     C#     D      D#     E      E#      F     G     G#     A      A#     B
+    0x0116,0x0127,0x0138,0x014b,0x015e,0x0173,0x0189,0x01a1,0x01ba,0x01d4,0x01f0,0x020d, // 1
+    0x022c,0x024e,0x0271,0x0296,0x02bd,0x02e7,0x0313,0x0342,0x0374,0x03a8,0x03e0,0x041b, // 2
+    0x0459,0x049c,0x04e2,0x052c,0x057b,0x05ce,0x0627,0x0684,0x06e8,0x0751,0x07c0,0x0836, // 3
+    0x08b3,0x0938,0x09c4,0x0a59,0x0af6,0x0b9d,0x0c4e,0x0d09,0x0dd0,0x0ea2,0x0f81,0x106d, // 4
+    0x1167,0x1270,0x1388,0x14b2,0x15ed,0x173a,0x189c,0x1a13,0x1ba0,0x1d44,0x1f02,0x20da, // 5
+    0x22ce,0x24e0,0x2711,0x2964,0x2bda,0x2e75,0x3138,0x3426,0x3740,0x3a89,0x3e04,0x41b4, // 6
+    0x459c,0x49c0,0x4e22,0x52c8,0x57b4,0x5ceb,0x6271,0x684c,0x6e80,0x7512,0x7c08,0x8368, // 7
+    0x8b38,0x9380,0x9c45,0xa590,0xaf68,0xb9d6,0xc4e3,0xd098,0xdd00,0xea24,0xf810         // 8
+};
+
+// small sine table used from tremelo effects etc
+static const signed char sinTable[] = { // 360deg +- 64
+    0,  12,  24,  35,  45,  53,  59,  62,  64,  62,  59,  53,  45,  35,  24,  12,
+    0, -12, -24, -35, -45, -53, -59, -62, -64, -62, -59, -53, -45, -35, -24, -12
+};
+
+unsigned long dataPins[256];
+unsigned long addrPins[32];
+void /*__iomem*/ * gpio, * gpio_clock, * gpio_timer;
+struct bcm2835_peripheral gpioPlease = {GPIO_BASE};
+Synth synth;
+
+void iowrite32(unsigned long value, unsigned long * addr) {
+    addr[0] = value;
+}
+
+unsigned long ioread32(unsigned long * addr) {
+    return addr[0];
+}
+
+int delayValue = 0;
+void sidDelay() {
+    for (int x=0;x<2000;x++) {
+        delayValue++; // do something that wont be optimized away
+    }
+}
+
+void writeSid(int reg, int val) {
+    val &= 0xff;
+    iowrite32((unsigned long) addrPins[reg % 32], (unsigned long *) gpio + 7);
+    iowrite32((unsigned long) ~addrPins[reg % 32] & addrPins[31], (unsigned long *) gpio + 10);
+    iowrite32((unsigned long) 1 << CS, (unsigned long *) gpio + 10);
+    iowrite32((unsigned long) dataPins[val % 256], (unsigned long *) gpio + 7);
+    iowrite32((unsigned long) ~dataPins[val % 256] & dataPins[255], (unsigned long *) gpio + 10);
+    sidDelay();
+    iowrite32((unsigned long) 1 << CS, (unsigned long *) gpio + 7);
+    sidDelay();
+}
+
+void startSidClock(int freq) {
+    int divi = 19200000 / freq;
+    int divr = 19200000 % freq;
+    int divf = (int) (divr * 4096 / 19200000);
+
+    if (divi > 4095) {
+        divi = 4095;
+    }
+    iowrite32(BCM_PASSWORD | GPIO_CLOCK_SOURCE, (unsigned long *) gpio_clock + 28);
+    while ((ioread32((unsigned long *)gpio_clock + 28) & 0x80) != 0);
+
+    iowrite32(BCM_PASSWORD | (divi << 12) | divf, (unsigned long *) gpio_clock + 29);
+    iowrite32(BCM_PASSWORD | 0x10 | GPIO_CLOCK_SOURCE, (unsigned long *) gpio_clock + 28);
+    iowrite32(0x0000280, (unsigned long *) gpio_timer + TIMER_CONTROL);
+    iowrite32(0x00000F9, (unsigned long *) gpio_timer + TIMER_PRE_DIV);
+}
+
+void setPinsToOutput(void) {
+    int i, fSel, shift;
+
+    for (i = 0; i < 8; i++) {
+        fSel = gpioToGPFSEL[DATA[i]];
+        shift = gpioToShift[DATA[i]];
+        iowrite32(ioread32((unsigned long *) gpio + fSel) & ~(7 << shift)  | (1 << shift), (unsigned long *) gpio + fSel);
+    }
+
+    for (i = 0; i < 5; i++) {
+        fSel = gpioToGPFSEL[ADDR[i]];
+        shift = gpioToShift[ADDR[i]];
+        iowrite32(ioread32((unsigned long *) gpio + fSel) & ~(7 << shift)  | (1 << shift), (unsigned long *) gpio + fSel);
+    }
+
+    fSel = gpioToGPFSEL[CS];
+    shift = gpioToShift[CS];
+    iowrite32(ioread32((unsigned long *) gpio + fSel) & ~(7 << shift) | (1 << shift), (unsigned long *) gpio + fSel);
+
+    fSel = gpioToGPFSEL[RW];
+    shift = gpioToShift[RW];
+    iowrite32(ioread32((unsigned long *) gpio + fSel) & ~(7 << shift) | (1 << shift), (unsigned long *) gpio + fSel);
+
+    fSel = gpioToGPFSEL[RES];
+    shift = gpioToShift[RES];
+    iowrite32(ioread32((unsigned long *) gpio + fSel) & ~(7 << shift) | (1 << shift), (unsigned long *) gpio + fSel);
+
+    fSel = gpioToGPFSEL[CLK];
+    shift = gpioToShift[CLK];
+    iowrite32(ioread32((unsigned long *) gpio + fSel) & ~(7 << shift) | (4 << shift), (unsigned long *) gpio + fSel);
+}
+
+void sidReset() {
+    for (int i = 0; i < 23; i++) {
+        writeSid(i, 0);
+    }
+    writeSid(24, 15);
+}
+
+void generatePinTables(void) {
+    int i;
+
+    for (i = 0; i < 256; i++) {
+        dataPins[i] = (unsigned long) (i & 1) << DATA[0];
+        dataPins[i] |= (unsigned long) ((i & 2) >> 1) << DATA[1];
+        dataPins[i] |= (unsigned long) ((i & 4) >> 2) << DATA[2];
+        dataPins[i] |= (unsigned long) ((i & 8) >> 3) << DATA[3];
+        dataPins[i] |= (unsigned long) ((i & 16) >> 4) << DATA[4];
+        dataPins[i] |= (unsigned long) ((i & 32) >> 5) << DATA[5];
+        dataPins[i] |= (unsigned long) ((i & 64) >> 6) << DATA[6];
+        dataPins[i] |= (unsigned long) ((i & 128) >> 7) << DATA[7];
+    }
+
+    for (i = 0; i < 32; i++) {
+        addrPins[i] = (unsigned long) (i & 1) << ADDR[0];
+        addrPins[i] |= (unsigned long) ((i & 2) >> 1) << ADDR[1];
+        addrPins[i] |= (unsigned long) ((i & 4) >> 2) << ADDR[2];
+        addrPins[i] |= (unsigned long) ((i & 8) >> 3) << ADDR[3];
+        addrPins[i] |= (unsigned long) ((i & 16) >> 4) << ADDR[4];
+    }
+}
+
+int mapPeripheral(struct bcm2835_peripheral *p) {
+    if ((p->mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
+        printf("Failed to open /dev/mem, try checking permissions.\n");
+        return -1;
+    }
+
+    p->map = mmap(
+        NULL,
+        GPIO_BLOCK_SIZE,
+        PROT_READ|PROT_WRITE,
+        MAP_SHARED,
+        p->mem_fd,      // File descriptor to physical memory virtual file '/dev/mem'
+        p->addr_p       // Address in physical map that we want this memory block to expose
+    );
+
+    if (p->map == MAP_FAILED) {
+        perror("mmap");
+        return -1;
+    }
+
+    p->addr = (volatile unsigned int *)p->map;
+    return 0;
+}
+
+void unmapPeripheral(struct bcm2835_peripheral *p) {
+    munmap(p->map, GPIO_BLOCK_SIZE);
+    close(p->mem_fd);
+}
+
+Voice * findVoiceForKey(int key) {
+    for (int x=0;x<TOTAL_VOICES;x++) {
+        Voice * voice = &synth.voiceTable[x];
+        if (voice->key == key) {
+            return voice;
+        }
+    }
+    return 0;
+}
+
+void setNoteOff(int key) {
+    Voice * voice;
+    if ((voice = findVoiceForKey(key)) != 0) {
+        voice->setVoiceOff();
+    }
+}
+
+void setSustain() {
+    for (int x=0;x<TOTAL_VOICES;x++) {
+        Voice * voice = &synth.voiceTable[x];
+        if (voice->velocity != 0) {
+            voice->sustain = synth.sustain;
+        }
+    }
+}
+
+void releaseSustain() {
+    for (int x=0;x<TOTAL_VOICES;x++) {
+        Voice * voice = &synth.voiceTable[x];
+        if (voice->sustain != 0 && voice->velocity != 0) {
+            voice->setVoiceOff();
+        }
+    }
+}
+
+Voice * findVoice() {
+    if (++synth.nextVoice >= TOTAL_VOICES) {
+        synth.nextVoice = 0;
+    }
+
+    for (int x=0;x<TOTAL_VOICES;x++) {
+        int v = synth.nextVoice + x;
+        if (v >= TOTAL_VOICES) {
+            v -= TOTAL_VOICES;
+        }
+        Voice * voice = &synth.voiceTable[v];
+        if (voice->velocity == 0) {
+            return voice;
+        }
+    }
+    return (Voice*)&synth.voiceTable[ synth.nextVoice ];
+}
+
+void Voice::setNoteOn(int key, int velocity) {
+    // set up voice
+    this->key = key;
+    this->sustain = synth.sustain; // remember sustain state when key was pressed
+    this->velocity = velocity;
+    this->frame = 0;
+
+    // ADSR
+    writeSid(this->offset + REGISTER_AD,
+            (synth.instrument.velocityFunction & VELOCITY_ATTACK) ?
+                    (0xf - ((velocity>>3)<<4)) | (synth.instrument.attackDecay & 0x0f) :
+                    synth.instrument.attackDecay ); // attack/decay
+    writeSid(this->offset + REGISTER_SR,
+            synth.instrument.sustainRelease); // sustain/release
+
+    // CONTROL
+    writeSid(this->offset + REGISTER_CONTROL, synth.instrument.control | VOICE_GATE);
+
+    updateVoice();
+}
+
+void Voice::updateVoice() {
+    int freq = noteTable[ this->key ];
+
+    this->frame++;
+
+    // adjust freq based on pitch wheel
+    freq += (synth.pitch >> 2);
+    // do tremelo if needed
+    if (synth.modulation > 0) {
+        freq += (sinTable[ (synth.frame * (synth.modulation >> 3)) & 31 ] >> 1);  // >> 3 = Zxxxxxxx -> 000Zxxxx
+    }
+
+    int off = this->offset;
+
+    writeSid(off + REGISTER_FREQ_LO, freq & 0xff);
+    writeSid(off + REGISTER_FREQ_HI, (freq >> 8) & 0xff);
+    if (synth.instrument.control & VOICE_PULSE) {
+        int pw = ((synth.instrument.velocityFunction & VELOCITY_PULSEWIDTH) ? this->velocity << 4 : this->frame << 4);
+        writeSid(off + REGISTER_PW_LO, pw & 0xff);
+        writeSid(off + REGISTER_PW_HI, (pw >> 8) & 0xff);
+    }
+}
+
+void Voice::setVoiceOff() {
+    this->velocity = 0;
+    this->key = 0;
+    this->sustain = 0;
+    writeSid(this->offset+REGISTER_CONTROL, synth.instrument.control & VOICE_CLOSEGATE);
+}
+
+void setupSnyth() {
+    memset(&synth, 0, sizeof(synth));
+
+    for (int x=0;x<TOTAL_VOICES;x++) {
+        Voice * voice = (Voice*)&synth.voiceTable[x];
+        voice->offset = x * 7;
+    }
+
+    synth.instrument.control = VOICE_TRIANGE;
+    synth.instrument.attackDecay = 0x7c;
+    synth.instrument.sustainRelease = 0x33;
+
+    synth.channel = 0;
+    synth.volume = 15;
+    synth.modulation = 64;
+}
+
+void injectMidi(int command, int data1, int data2) {
+    int channel = command & 0x0f;
+    int func = command >> 4;
+
+    printf("channel: %d func: %d note: %d velocity: %d\n", channel, func, data1, data2);
+
+    if (channel != synth.channel) {
+        return;
+    }
+    switch (func) {
+        case MIDI_NOTEOFF:
+            setNoteOff(data1);
+            break;
+        case MIDI_NOTEON:
+            if (data2 == 0) {
+                setNoteOff(data1);
+            } else if (data1 < 95) {
+                findVoice()->setNoteOn(data1, data2);
+            }
+            break;
+        case MIDI_CONTROL:
+            switch (data1) {
+                case 0:
+                    sidReset();
+                    return;
+                case MIDI_CONTROL_MODULATION:
+                    synth.modulation = data2;
+                    break;
+                case MIDI_CONTROL_VOLUME:
+                    writeSid(REGISTER_VOLUME, (synth.volume = data2 >> 3) );  // 0xxxxxxx -> 0000xxxx
+                    break;
+                case MIDI_CONTROL_SUSTAIN:
+                    if ((synth.sustain = data2) != 0) {
+                        setSustain();
+                    } else {
+                        releaseSustain();
+                    }
+                    break;
+                case MIDI_CONTROL_REVERB:
+                    synth.reverb = data2;
+                    break;
+            }
+            break;
+        case MIDI_PITCHWHEEL:
+            synth.pitch = ((data1 | (data2 << 8)) - 16384);
+            for (int x=0;x<TOTAL_VOICES;x++) {
+                synth.voiceTable[x].updateVoice();
+            }
+            break;
+        case MIDI_PROGRAM:
+            setupSnyth();
+            sidReset();
+            break;
+    }
+}
+
+int main() {
+    mapPeripheral(&gpioPlease);
+    gpio = (void*)gpioPlease.addr;
+
+    generatePinTables();
+    setPinsToOutput();
+    sidReset();
+    setupSnyth();
+
+    FILE * fp = fopen("/dev/snd/midiC1D0","r");
+    if( fp == NULL ) {
+        perror("error opening MIDI device\n");
+        return 0;
+    }
+
+    for (;;) {
+        int command = fgetc(fp);
+        if (command & MIDI_COMMANDBIT) {
+            injectMidi(command, fgetc(fp), fgetc(fp));
+        }
+    }
+}
+
+struct sockaddr_in clientAddress;
+struct sockaddr_in serverAddress;
+
+void readFromSocket() {
+    int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd < 0) {
+        printf("fail sockfd\n");
+        return;
+    }
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(1111);
+
+    if (bind(socketfd, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0) {
+        printf("socket listener failed to bind\n");
+        return;
+    }
+
+    listen(socketfd,5);
+    socklen_t clilen = sizeof(clientAddress);
+    socketfd = accept(socketfd, (struct sockaddr *) &clientAddress, &clilen);
+
+    if (socketfd < 0) {
+        printf("no FD\n");
+        return;
+    }
+
+    printf("socket accepted\n");
+
+    unsigned char buffer[2];
+    for (; ;) {
+        int n = read(socketfd, buffer, 2);
+        if (n < 0) {
+            perror("error reading from socket");
+            return;
+        }
+        if (n == 1) {
+            read(socketfd, &buffer[1], 1);
+        }
+        writeSid(buffer[0], buffer[1]);
+    }
+    //return 0;
+}
+
---
+++ monty.h
@@ -0,0 +1,157 @@
+
+#ifndef _MONTY_H
+#define _MONTY_H
+
+#define VOICE_NOISE  128
+#define VOICE_PULSE  64
+#define VOICE_SAWTOOTH 32
+#define VOICE_TRIANGE 16
+#define VOICE_TEST 8 // not to be included in Instrument definitions
+#define VOICE_RINGMOD 4
+#define VOICE_SYNC 2
+#define VOICE_GATE 1 // not to be included in Instrument definitions
+
+#define VOICE_CLOSEGATE 254
+
+// register groups offset at SID addresses 0,7 and 14
+#define REGISTER_FREQ_LO 0
+#define REGISTER_FREQ_HI 1
+#define REGISTER_PW_LO   2
+#define REGISTER_PW_HI   3
+#define REGISTER_CONTROL 4
+#define REGISTER_AD      5
+#define REGISTER_SR      6
+
+#define REGISTER_VOLUME 24
+#define VELOCITY_PULSEWIDTH 1
+#define VELOCITY_ATTACK 2
+#define PULSEWIDTH_VELOCITY 1
+#define PULSEWIDTH_SWEEP 2
+
+// GPIO
+#define BCM2708_PERI_BASE       0x20000000
+#define GPIO_BASE               (BCM2708_PERI_BASE + 0x200000)
+#define GPIO_TIMER              (BCM2708_PERI_BASE + 0x003000)
+#define GPIO_CLOCK              (BCM2708_PERI_BASE + 0x00101000)
+#define TIMER_OFFSET            (4)
+#define GPIO_BLOCK_SIZE         (4*1024)
+#define BCM_PASSWORD            0x5A000000
+#define GPIO_CLOCK_SOURCE       1
+#define TIMER_CONTROL           (0x408 >> 2)
+#define TIMER_IRQ_RAW           (0x410 >> 2)
+#define TIMER_PRE_DIV           (0x41C >> 2)
+
+#define SID_HZ 1000000
+
+#define CS  18
+#define RW  0
+#define RES 0
+#define CLK 4
+
+#define TOTAL_VOICES    3
+
+struct bcm2835_peripheral {
+    unsigned long addr_p;
+    int mem_fd;
+    void * map;
+    volatile unsigned int * addr;
+};
+
+#define MIDI_COMMANDBIT            (8<<4)
+
+// MIDI HI
+#define MIDI_NOTEOFF               8
+//1000 0000
+#define MIDI_NOTEON                9
+//1001 0000
+#define MIDI_POLYPHONICAFTERTOUCH  10
+//1010 0000
+#define MIDI_CONTROL               11
+//1011 0000
+#define MIDI_PROGRAM               12
+//1100 0000
+#define MIDI_CHANNELAFTERTOUCH     13
+//1101 0000
+#define MIDI_PITCHWHEEL            14
+//1110 0000
+#define MIDI_SYSEX                 15
+//1111 0000
+
+#define MIDI_CONTROL_MODULATION            1
+#define MIDI_CONTROL_VOLUME                7
+#define MIDI_CONTROL_SUSTAIN               64
+#define MIDI_CONTROL_REVERB                91
+
+class Instrument {
+public:
+    int control;			    // waveform control register
+    int filterFlags;		  // filter flags
+    int pulseWidth;		  // inital pulse width
+    int attackDecay;		  // A/D
+    int sustainRelease;  // S/R
+    int freqCutoff;		  // filter cutoff
+    int resData;
+    int resMode;
+    // extra parameters, midi standard range 0-127
+    int defaultModulation;	  // 0 = disable, [midi = chorus]
+    int velocityFunction;
+    int pulseWidthFunction;
+};
+
+class Voice {
+public:
+    int offset;          // offset to the 3 identicle sets of voice registers (0, 7 or 14)
+    int key;
+    int velocity;
+    int sustain;			// sustain flag (voice wont be stoped until sustain cleared)
+    int frame;		  	    // frame counter for progressive effects
+
+    void setNoteOn(int key, int velocity);
+    void updateVoice();
+    void setVoiceOff();
+};
+
+class Synth {
+public:
+    int frame;
+    int channel;     // the channel we are listening for
+    Instrument instrument;
+    int pitch;       // pitch wheel position
+    int modulation;  // 64 = center
+    int reverb;
+    int sustain;
+    int volume;
+    int nextVoice;
+    Voice voiceTable[3];
+};
+
+/*
+int mapGPIO(void) {
+    unsigned long mem;
+
+//    mem = request_mem_region(GPIO_BASE, 4096, "gpio");
+//    if (mem == NULL) {
+//        print("Cannot get GPIO");
+//        return -1;
+//    }
+//    gpio = ioremap(GPIO_BASE, 4096);
+
+//    mem = request_mem_region(GPIO_CLOCK, 32, "gpioclk");
+//    if (mem == NULL) {
+//        print("Cannot get GPIO Clock");
+//        return -1;
+//    }
+//    gpio_clock = ioremap(GPIO_CLOCK, 32);
+
+//    mem = request_mem_region(GPIO_TIMER, 256, "gpiotimer");
+//    if (mem == NULL) {
+//        print("Cannot get GPIO timer");
+//        return -1;
+//    }
+//    gpio_timer = ioremap(GPIO_TIMER, 256);
+
+    return 0;
+}
+ */
+
+#endif
